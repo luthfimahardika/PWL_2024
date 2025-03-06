b24fe7e34fa4f82bf5db62fbf825f33e931b024a
Luthfi Mahardika <lutfi.mahardika17@gmail.com>
2025-03-06 11:16:11 +0700

    web-praktikum1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/selamat', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2341760181 - Luthfi Putra Mahardika';
+});
+
+Route::get('/user/{luthfi}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($articlesid) {
+    return 'Halaman Artikel dengan ID '.$articlesid;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
